92d96523c0d89ece163509523c9dde51baaffd25
sayag11 <sayag11@gmail.com>
2015-08-15 14:08:14 +0300

    progress in load file function + added sub-function

--- ChessProject/SettingState.c
+++ ChessProject/SettingState.c
@@ -3,11 +3,13 @@
 #include <assert.h>
 #include <stdlib.h>
 #include "Chess.h"
+#include <windows.h>
 
 int game_mode=1;
 int difficulty = 1;
 char* next_player = "white";
 char* user_color = "white"; 
+#define XML_ERROR "Error:the xml file is not in the correct format"
 
 
 void setGameMode(int mode)
@@ -17,7 +19,7 @@ void setGameMode(int mode)
 }
 
 // sets the difficulty ( only in player vs AI mode )
-void setDifficulty(char* diff)
+void setDifficulty(int diff)
 {
 	difficulty = diff;
 }
@@ -38,12 +40,35 @@ void loadGame(char* fileName)
 			//do nothing . just skipping first 2 lines
 		}
 	}
-	while ((fgets(line,sizeof(file), file)) != NULL) {
-		printf("%s", &line); // for tests
-		// do the actual reading here
-	}
-}
+	//reading line 3
+	fgets(line, sizeof(file) * 50, file);
+	setNextPlayer(findSubstring(line, "white", "black"));
+	printf(next_player);
+	Sleep(1000);
+	// reading line 4
+	fgets(line, sizeof(file) * 10, file);
+	setGameMode(atoi(findSubstring(line, "1", "2")));
+	Sleep(1000);
+	// reading line 5
+	fgets(line, sizeof(file) * 10, file);
 
+
+}
+char* findSubstring(char* line,char* opt1, char* opt2){
+	if (strstr(line, opt1) != NULL){
+		return opt1;
+	}
+	else{
+		if (strstr(line, opt2) != NULL){
+			return opt2;
+		}
+		else{
+			printf(XML_ERROR);
+			//TODO EXIT
+		}
+	}
+	return NULL;
+}
 //remove all pieces from the board
 void clearBoard()
 {
